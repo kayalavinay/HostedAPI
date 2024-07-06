5becd469aa4fefa6d66c220997590c86354c9046
Vinay <kayalavinay14@gmail.com>
2024-07-06 22:07:36 +0530

    Add project files.

---
+++ Controllers/ImageController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HostedAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ImageController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            string response = "Vinay";
+            return Ok(response);
+        }
+
+        [HttpPost]
+        public IActionResult Validate(string userName,string Password)
+        {
+            bool isvalidate = false;
+            if(userName == "vinay" && Password == "vinnu")
+                isvalidate = true;
+            else if (userName == "vamsi" && Password == "vamsi")
+                isvalidate= true;
+            else
+                isvalidate = false;
+            return Ok(isvalidate);
+        }
+    }
+}
